a691d818a4b1e66e2b3cfa01149ce6672474542c
Shahriar12345123 <rifatbakta11@gmail.com>
2025-08-22 23:38:40 +0600

    First time

---
+++ Leson-1/Rifat.java
@@ -0,0 +1,32 @@
+
+public class Rifat{
+   static  int myAge = 20;
+   static  int myWeight = 50;
+   static int myFriendAge = 50;
+   static String name = "Naim";
+   static String firstName = "Johny";
+    static String lastName = "Sinc";
+    static String fullName = firstName + " " +lastName;
+    static int x = 10;
+    static int y = 20;
+    public static void main(String[] args){
+        System.out.println( "Hellow ! I am Rifat");
+        System.out.println( 20*504) ;
+        System.out.print(" Hellow world");
+        System.out.print(" Hellow world");
+        System.out.print(" Hellow world");
+        System.out.print(" Hellow world");
+        System.out.println(" Hellow world");
+        System.out.println(" Hellow world");
+        System.out.println((20+3) + " "  +(40-20) + " "+(2*903654)); 
+        System.out.println(myWeight);
+        System.out.print(myAge);
+        System.out.println( "My age is:"+ (myAge) + ","+ " My Weight is :" +(myWeight)+ "."  );
+        System.out.println(" My friend name is same . his age is:"+ (myFriendAge )+ "," + "My age is:"+(myAge)+ ".");
+        System.out.println("My name is : " + name) ;
+        System.out.println( " My name is: " + name +   " , My age is :"+ (myAge) + "." );
+        System.out.println( "My friend name is " + fullName + " ." );
+        System.out.println( x + y );
+    }
+}
+
