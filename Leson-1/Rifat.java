c2b6914dbe85012404a7619a843e3a50ce623213
Shahriar12345123 <rifatbakta11@gmail.com>
2025-08-25 09:33:05 +0600

    F

--- Leson-1/Rifat.java
+++ Leson-1/Rifat.java
@@ -9,7 +9,35 @@ public class Rifat{
     static String fullName = firstName + " " +lastName;
     static int x = 10;
     static int y = 20;
+    static int a =20 , b = 50 , c = 100 ;
+    static float height = 5.6f;
+    static char grade = 'A';
+    static boolean mybool = !true;
+    static byte  myByte = 125;
+    static short myShort = 30000;
+    static long myLong = 12345678954641L;
+    static int utop = 50;
+    static int mix = 289;
+    // note write pen and khata 
+    static int sum = 50;
+    static int xox = 70;
+    static int xoxss = 75;
+    static int yoy = 35;
+    static int utos = 100;
+
+     
+    
     public static void main(String[] args){
+        utop++;
+        mix--;
+        utos++;
+        xox--;
+        System.out.println( utos );
+        System.out.println( xox );
+        System.out.println( sum % xox );
+        System.out.println( xoxss % yoy );
+        System.out.println(utos % yoy );
+        System.out.println(xoxss * yoy );
         System.out.println( "Hellow ! I am Rifat");
         System.out.println( 20*504) ;
         System.out.print(" Hellow world");
@@ -27,6 +55,18 @@ public class Rifat{
         System.out.println( " My name is: " + name +   " , My age is :"+ (myAge) + "." );
         System.out.println( "My friend name is " + fullName + " ." );
         System.out.println( x + y );
+        System.out.println(a + b + c );
+        System.out.println("My height is : " + height + " feet .");
+        System.out.println("My grade is : " + grade + " .");
+        System.out.println("My bool is : " + mybool + " .");
+        System.out.println("My byte is : " + ++myByte + " .");
+        System.out.println("My short is : " + ++myShort  + " .");
+        System.out.println("My long is : " + myLong++ + " .");
+        System.out.println( utop);
+        System.out.println( mix);
+        System.out.print(mix % utop);
+        System.out.println( mix*utop ) ;
+
     }
 }
 
